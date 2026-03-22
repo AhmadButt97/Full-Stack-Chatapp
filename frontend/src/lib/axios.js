7523d8fb774273edd102fd591c5770563282616f
Ahmadbutt <ahmadbutt97.14@gmail.com>
2026-03-22 23:33:43 +0500

    Added full stack chatapp with K8s deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,3 +4,12 @@ export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
+
+// 👇 ADD THIS BLOCK HERE
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
